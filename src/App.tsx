30bfc24e0bb8e449de679ff9b21b2801bf26ba8f
dani voro <mr.voro@gmail.com>
2023-07-09 09:12:00 +0200

    finished the first version of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import { greet } from "./utils/greet";
+import React, { useState } from 'react';
+import './App.css'
 
-function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+function App() {
+  const [paralelNumbers, setParalelNumbers] = useState <number[]>([1])
+
+ 
+  function generateParalelNumbers(){
+    function nextParalelNumber(){
+      return paralelNumbers[paralelNumbers.length - 1] + 1
+    }
+    setParalelNumbers([...paralelNumbers, nextParalelNumber()])
+  }
+
+
+  function arrayToFizzBuzz(parallelNumbers: number[]): Array<string | number> {
+    function fizzBuzz(num: number): string | number {
+      if (num % 3 === 0 && num % 5 !== 0) {
+        return 'Fizz';
+      }
+      if (num % 5 === 0 && num % 3 !== 0) {
+        return 'Buzz';
+      }
+      if (num % 3 === 0 && num % 5 === 0) {
+        return 'FizzBuzz';
+      } else {
+        return num;
+      }
+    }
+    return parallelNumbers.map((x) => fizzBuzz(x));
+  }
+  
+
+  return (
+    <>
+      <p>{arrayToFizzBuzz(paralelNumbers).join(' ')}</p>
+      <button onClick={generateParalelNumbers}>Next</button>
+    </>
+  )
 }
 
 export default App;
